2f40bd91cbbe46ac42a5ced55701c2f9a4cbcd7d
knostijr <christophkonst@icloud.com>
2026-03-13 14:12:32 +0100

    Connect quiz API URLs

--- core/urls.py
+++ core/urls.py
@@ -8,4 +8,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts_app.api.urls')),
+    path('api/', include('quiz_app.api.urls')),
 ]
---
+++ quiz_app/api/urls.py
@@ -0,0 +1,15 @@
+"""
+URL routing for quiz API.
+"""
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import QuizViewSet
+
+
+router = DefaultRouter()
+router.register('quizzes', QuizViewSet, basename='quiz')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
